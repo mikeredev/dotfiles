dafe13406000b2089386b03acf09ec5698af0f62
mikeredev <mikeredev@gmail.com>
2024-01-16 04:54:07 +0000

    refactor i3blocks

--- i3blocks/styles/style.py
+++
@@ -1,9 +0,0 @@
-OK          = "#ffffff"     # normal operations
-WARN        = "#ffa500"     # take care
-NOK         = "#e74c3c"     # alert!
-ACTIVE      = "#a9a9a9"     # device on/high
-INACTIVE    = "#999999"     # device on/low
-NONE        = "#666666"     # device off / device not found
-
-FONT        = "Inconsolata Bold 11"             # font used for returned values
-GLYPHS      = "Font Awesome 6 Free Regular 11"  # font used for glyphs
